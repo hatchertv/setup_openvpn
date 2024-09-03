3bc35a5daa7dbcba94a1c1d07c005d1403126f64
hatchertv <hatcher.travis@gmail.com>
2024-09-03 12:36:54 -0500

    Update setup_openvpn.sh
    Now defaults to ipv4, previously used ipv6 by default

--- setup_openvpn.sh
+++ setup_openvpn.sh
@@ -10,7 +10,7 @@ sudo rm -rf /var/log/openvpn*
 
 # Reinstall OpenVPN to ensure a clean installation
 sudo apt-get update
-sudo apt-get install -y openvpn easy-rsa curl
+sudo apt-get install -y openvpn easy-rsa
 
 # Set up the OpenVPN server configuration directory
 sudo mkdir -p /etc/openvpn
@@ -54,11 +54,11 @@ fi
 # Move generated keys and certificates to the OpenVPN directory
 sudo cp pki/ca.crt pki/dh.pem pki/issued/server.crt pki/private/server.key /etc/openvpn/
 
-# Automatically determine the public IP address
-PUBLIC_IP=$(curl -s ifconfig.me)
-
-# Set the MTU size
-MTU_SIZE=1432
+# Calculate the optimal MTU for the server
+MTU=$(ping -c 1 -M do -s 1432 google.com 2>/dev/null | grep -oP '(?<=MTU\s)\d+')
+if [ -z "$MTU" ]; then
+    MTU=1400  # Fallback MTU if the calculation fails
+fi
 
 # Create the server configuration file
 cat << EOF | sudo tee /etc/openvpn/server.conf
@@ -66,15 +66,20 @@ port 443
 proto tcp
 dev tun
 topology subnet
+local $(curl -s http://checkip.amazonaws.com)  # Automatically determine the public IP address
 ca ca.crt
 cert server.crt
 key server.key
 dh dh.pem
 tls-auth ta.key 0
 server 10.8.0.0 255.255.255.0
+mtu $MTU
+mssfix $((MTU-40))
 push "redirect-gateway def1 bypass-dhcp"
 push "dhcp-option DNS 8.8.8.8"
 push "dhcp-option DNS 8.8.4.4"
+push "mtu $MTU"
+push "mssfix $((MTU-40))"
 keepalive 10 120
 data-ciphers AES-256-GCM:AES-128-GCM:CHACHA20-POLY1305:AES-256-CBC
 user nobody
@@ -87,7 +92,6 @@ verb 6
 plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
 verify-client-cert none
 username-as-common-name
-mssfix $MTU_SIZE
 EOF
 
 # Configure IP forwarding
@@ -95,14 +99,11 @@ echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 sudo sed -i '/net.ipv4.ip_forward/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
 sudo sysctl -p
 
-# Get the network interface name dynamically
-INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
-
-# Set up firewall rules with NAT for IP masquerading
-sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $INTERFACE -j MASQUERADE
+# Set up firewall rules
 sudo ufw allow 443/tcp
 sudo ufw allow OpenSSH
-echo "y" | sudo ufw enable
+sudo ufw disable
+sudo ufw enable
 
 # Start and enable the OpenVPN service
 sudo systemctl start openvpn@server
@@ -112,20 +113,16 @@ if [ $? -ne 0 ]; then
 fi
 sudo systemctl enable openvpn@server
 
-# Generate a unique filename for the client.ovpn file
-UNIQUE_ID=$(date +%s)
-CLIENT_CONFIG_FILENAME="client_${UNIQUE_ID}.ovpn"
-
 # Set the home directory for the client.ovpn file based on the user running the script
 USER_HOME=$(eval echo ~${SUDO_USER})
-CLIENT_CONFIG_PATH="${USER_HOME}/${CLIENT_CONFIG_FILENAME}"
+CLIENT_CONFIG_PATH="${USER_HOME}/$(hostname -s)_client.ovpn"
 
 # Generate the client configuration file
 cat << EOF > ${CLIENT_CONFIG_PATH}
 client
 dev tun
 proto tcp
-remote $PUBLIC_IP 443
+remote $(curl -s http://checkip.amazonaws.com) 443
 resolv-retry infinite
 nobind
 user nobody
@@ -136,6 +133,8 @@ remote-cert-tls server
 auth-user-pass
 cipher AES-256-CBC
 verb 3
+mtu $MTU
+mssfix $((MTU-40))
 <ca>
 $(sudo cat /etc/openvpn/ca.crt)
 </ca>
@@ -151,9 +150,7 @@ $(sudo cat /etc/openvpn/ta.key)
 key-direction 1
 EOF
 
-# Print out the location of the client configuration file
+# Print out the location of the client configuration file and SCP command
 echo "OpenVPN server setup complete. The client configuration file is available as ${CLIENT_CONFIG_PATH}."
-
-# Provide the user with an scp command to download the configuration file
-echo "You can download the client configuration file using the following command:"
-echo "scp ${USER}@${PUBLIC_IP}:${CLIENT_CONFIG_PATH} ."
+echo "You can download it using the following SCP command:"
+echo "scp ${USER}@$(curl -s http://checkip.amazonaws.com):${CLIENT_CONFIG_PATH} ."
