09bf17c02a8b9ec93c3b46cc0c2b251c9ade6009
hatchertv <hatcher.travis@gmail.com>
2024-09-03 19:27:04 -0500

    Update setup_openvpn.sh

--- setup_openvpn.sh
+++ setup_openvpn.sh
@@ -11,46 +11,6 @@ determine_primary_interface() {
     fi
 }
 
-# Function to configure OpenVPN server
-configure_openvpn_server() {
-    # Get the MTU from the primary interface
-    PRIMARY_MTU=$(ifconfig $PRIMARY_INTERFACE | grep -i mtu | awk '{print $4}')
-    VPN_MTU=$((PRIMARY_MTU - 50))
-
-    echo "Primary MTU: $PRIMARY_MTU"
-    echo "VPN MTU: $VPN_MTU"
-
-    # Create the OpenVPN server configuration file
-    cat << EOF | sudo tee /etc/openvpn/server.conf
-port 443
-proto tcp
-dev tun
-tun-mtu $VPN_MTU
-topology subnet
-ca /etc/openvpn/ca.crt
-cert /etc/openvpn/server.crt
-key /etc/openvpn/server.key
-dh /etc/openvpn/dh.pem
-tls-auth /etc/openvpn/ta.key 0
-server 10.8.0.0 255.255.255.0
-push "redirect-gateway def1 bypass-dhcp"
-push "dhcp-option DNS 8.8.8.8"
-push "dhcp-option DNS 8.8.4.4"
-keepalive 10 120
-data-ciphers AES-256-GCM:AES-128-GCM:CHACHA20-POLY1305:AES-256-CBC
-user nobody
-group nogroup
-persist-key
-persist-tun
-status /var/log/openvpn-status.log
-log /var/log/openvpn.log
-verb 6
-plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
-verify-client-cert none
-username-as-common-name
-EOF
-}
-
 # Function to configure IP forwarding and NAT
 configure_routing() {
     echo "Enabling IP forwarding..."
@@ -62,10 +22,6 @@ configure_routing() {
     sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $PRIMARY_INTERFACE -j MASQUERADE
     sudo iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
     sudo iptables -A FORWARD -s 10.8.0.0/24 -j ACCEPT
-
-    # Save the iptables rules
-    sudo apt-get install -y iptables-persistent
-    sudo netfilter-persistent save
 }
 
 # Function to install and configure OpenVPN
@@ -109,12 +65,38 @@ install_openvpn() {
     sudo cp pki/ca.crt pki/dh.pem pki/issued/server.crt pki/private/server.key /etc/openvpn/
 
     # Configure OpenVPN server
-    configure_openvpn_server
+    cat << EOF | sudo tee /etc/openvpn/server.conf
+port 443
+proto tcp
+dev tun
+ca /etc/openvpn/ca.crt
+cert /etc/openvpn/server.crt
+key /etc/openvpn/server.key
+dh /etc/openvpn/dh.pem
+tls-auth /etc/openvpn/ta.key 0
+server 10.8.0.0 255.255.255.0
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+keepalive 10 120
+cipher AES-256-CBC
+user nobody
+group nogroup
+persist-key
+persist-tun
+status /var/log/openvpn-status.log
+log /var/log/openvpn.log
+verb 3
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+verify-client-cert none
+username-as-common-name
+EOF
 
     # Configure routing
     configure_routing
 
     # Enable and start the OpenVPN service
+    sudo systemctl daemon-reload
     sudo systemctl start openvpn@server
     sudo systemctl enable openvpn@server
 }
@@ -141,7 +123,6 @@ remote-cert-tls server
 auth-user-pass
 cipher AES-256-CBC
 verb 3
-tun-mtu $VPN_MTU
 <ca>
 $(sudo cat /etc/openvpn/ca.crt)
 </ca>
@@ -160,7 +141,7 @@ EOF
     # Print out the location of the client configuration file
     echo "OpenVPN server setup complete. The client configuration file is available as ${CLIENT_CONFIG_PATH}."
     echo "To download the configuration file, use the following scp command:"
-    echo "scp ${SUDO_USER}@${PUBLIC_IP}:${CLIENT_CONFIG_PATH} ."
+    echo "scp ${SUDO_USER}@${PUBLIC_IP}:${CLIENT_CONFIG_PATH} ~/Downloads"
 }
 
 # Main function
